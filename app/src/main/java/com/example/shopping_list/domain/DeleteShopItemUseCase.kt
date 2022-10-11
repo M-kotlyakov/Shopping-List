8c81dcb97fdead92b28ab1b3aa5d36f39a198556
Max <m.kotlyakov.02@mail.ru>
2022-10-11 20:30:57 +0300

    add UseCases for domain layer

---
+++ app/src/main/java/com/example/shopping_list/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shopping_list.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
