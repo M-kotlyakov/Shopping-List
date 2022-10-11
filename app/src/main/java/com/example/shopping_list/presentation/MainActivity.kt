ff1ac137c05f10a5fb5e610362db3ca42f55113d
Max <m.kotlyakov.02@mail.ru>
2022-10-11 21:36:51 +0300

    add deleteShopItem, changeShopItem in MainViewModel. add LiveData in getShopList for transfer data between viewModel and View

--- app/src/main/java/com/example/shopping_list/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopping_list/presentation/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.shopping_list.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shopping_list.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivity", it.toString())
+            if (count == 0) {
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+                count++
+            }
+
+        }
     }
 }
